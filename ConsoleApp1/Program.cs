91b693652395ad4148dbaaba2a5c6238d2971ace
TechPrashanth <prasanthm@qompassnow.com>
2024-08-16 12:05:22 +0530

    Many Messages

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Prashanth!, How are you...");
+Console.WriteLine("Hello, Manoj!, How are you...");
+Console.WriteLine("Hello, Arjun!, How are you...");
+
