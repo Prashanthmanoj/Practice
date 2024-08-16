38161843c96e30bbada9a83292c54b269012fcd2
TechPrashanth <prasanthm@qompassnow.com>
2024-08-16 12:25:12 +0530

    I wrote a method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Prashanth!, How are you...");
-Console.WriteLine("Hello, Manoj!, How are you...");
-Console.WriteLine("Hello, Arjun!, How are you...");
+﻿myMethod();
 
+void myMethod()
+{
+    Console.WriteLine("Now i have a method");
+}
